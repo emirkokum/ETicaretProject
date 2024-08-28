cb7f94f1789d6e2775a111326d99eb4e2e774062
Emir Kokum <emir_kokum_@hotmail.com>
2024-08-29 00:22:28 +0300

    first commit

---
+++ ETicaretAPI/ETicaretAPI.Persistence/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    static class Configuration
+    {
+        static public string ConnectionString
+        {
+            get
+            {
+                var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json",false,false);
+                var config = builder.Build();
+                return config.GetConnectionString("PostgreSQL");
+
+            }
+        }
+    }
+}
